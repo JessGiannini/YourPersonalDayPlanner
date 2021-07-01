178e5a15b88e557bcffe4c779b64f0213775b9e2
JessGiannini <jessgiannini@gmail.com>
2021-07-01 02:31:20 -0700

    adding dates with moment

--- style.js
+++ style.js
@@ -1,6 +1,7 @@
 var timeEl = document.getElementById("#time");
 var scheduleEl = document.getElementById("#schedule");
 var lockEl = document.getElementById("#lock");
+// var todayEl = document.getElementById("#currentDay");
 
 var schedule = $("textarea");
 console.log(schedule);
@@ -9,3 +10,9 @@ var scheduleArray = Array.from(schedule);
 console.log(scheduleArray);
 
 var currentHour = moment().hour;
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMM Do"));
+
+//timeEl = today;
+console.log("date in header: " + timeEl);
+console.log("today's date" + today);

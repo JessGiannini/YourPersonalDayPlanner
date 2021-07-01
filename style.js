1461ac2a666175d6dc0e6dacb057847179b3fc38
JessGiannini <jessgiannini@gmail.com>
2021-07-01 02:54:27 -0700

    trying to figure out local storage

--- style.js
+++ style.js
@@ -1,18 +1,27 @@
 var timeEl = document.getElementById("#time");
 var scheduleEl = document.getElementById("#schedule");
 var lockEl = document.getElementById("#lock");
-// var todayEl = document.getElementById("#currentDay");
+var saveBtn = document.getElementById("#saveBtn");
 
 var schedule = $("textarea");
 console.log(schedule);
 
-var scheduleArray = Array.from(schedule);
-console.log(scheduleArray);
+// tutor suggested making schedule into an array to use for storing user input
+//var scheduleArray = Array.from(schedule);
+//console.log(scheduleArray);
 
 var currentHour = moment().hour;
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
-//timeEl = today;
-console.log("date in header: " + timeEl);
 console.log("today's date" + today);
+
+//TODO: create a way to check if current time is present time, past time, or future time.
+
+//TODO: save button index to lock it and creat hover
+//saveBtn.addEventListener("click", localStorage.setItem("schedule", "textarea"));
+saveBtn.addEventListener("storage", () => {
+  // When local storage changes, dump the list to
+  // the console.
+  console.log(JSON.parse(window.localStorage.getItem("textarea")));
+});
